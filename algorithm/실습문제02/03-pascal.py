c859d3f0d51590dcfb1abe520b63d232fd832a58
seonyoungan <asy093@naver.com>
2022-09-14 17:13:49 +0900

    feat: algorithm 22.09.14

--- algorithm/실습문제02/03-pascal.py
+++ algorithm/실습문제02/03-pascal.py
@@ -1,12 +1,9 @@
-class Solution(object):
-    def pascal(self, numRows):
-        poly = [[1]*(i+1) for i in range(numRows)]
-        for i in range(numRows):
-            for j in range(1, i):
-                poly[i][j] = poly[i-1][j-1] + poly[i-1][j]
-        return poly
+def pascal(self, numRows):
+    poly = [[1]*(i+1) for i in range(numRows)]
+    for i in range(numRows):
+        for j in range(1, i):
+            poly[i][j] = poly[i-1][j-1] + poly[i-1][j]
+    return poly
 
-
-solution = Solution()
 text = int(input("numRows = "))
-print(solution.pascal(text))
+print(pascal(text))
