c971ceab40f0a447027777fc508f47eb888527af
seonyoungan <asy093@naver.com>
2022-09-30 21:10:16 +0900

    rename: compiler folder name

--- compiler/hw2/main.c
+++ compiler/hw2/main.c
@@ -2,153 +2,217 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#define MAX 300
 
-#define NUMBER 256
-#define PLUS 257
-#define STAR 258
-#define LPAREN 259
-#define RPAREN 260
-#define END 261
-#define EXPRESSION 0
-#define TERM 1
-#define FACTOR 2
-#define ACC 999
+//token type
+enum {
+	NUMBER = 0, PLUS = 1, STAR = 2, LPAREN = 3, RPAREN = 4, END = 5, INIT = 6
+} token;
 
-int action[12][6] = {
-	{5, 0, 0, 4, 0, 0}, {0, 6, 0, 0, 0, ACC},{0, -2, 7, 0, -2, -2},
-	{0, -4, -4, 0, -4, -4}, {5, 0, 0, 4, 0, 0}, {0, -6, -6 ,0, -6, -6},
-	{5, 0, 0, 4, 0, 0}, {5, 0, 0, 4, 0, 0}, {0, 6, 0, 0, 11, 0},
-	{0, -1, 7, 0, -1, -1}, {0, -3, -3, 0, -3, -3}, {0, -5, -5, 0, -5, -5}
-};
 
-int go_to[12][3] = {
-	{1, 2, 3}, {0, 0, 0}, {0, 0, 0},
-	{0, 0, 0}, {8, 2, 3}, {0, 0, 0},
-	{0, 9, 3}, {0, 0, 10}, {0, 0, 0},
-	{0, 0, 0}, {0, 0, 0}, {0, 0, 0}
-};
+//data type intNum = int, floatNum = float
+typedef struct data {
+	enum {
+		intNum = 0,
+		floatNum = 1
+	} type;
+	union typeBox {
+		int i;
+		float f;
+	} value;
+}data;
 
-int prod_left[7] = { 0, EXPRESSION, EXPRESSION, TERM, TERM, FACTOR, FACTOR };
-int prod_length[7] = { 0, 3, 1, 3, 1, 3, 1 };
+//전역변수 선언
+data num;
+char input[MAX];
+int current = 0;
 
-int stack[1000];
-int value[250];
-int top = -1;
-int sym;
-int yyval;
+//함수의 원형
+void get_token();
+data expression();
+data term();
+data factor();
+int error(int i);
 
-int yyparse();
-int yylex();
-void shift(int i);
-void reduce(int i);
-void yyerror();
-
+//메인함수
 void main() {
-	yyparse();
-}
-
-int yyparse() {
-	int i;
-	stack[++top] = 0;
-	sym = yylex();
-	do {
-		i = action[stack[top]][sym - 256];
-		if (i == ACC)
-			printf("success %d!\n", value[top]);
-		else if (i > 0) {
-			shift(i);
-		}
-		else if (i < 0) {
-			reduce(-i);
-		}
-		else {
-			yyerror();
-		}
-	} while (i != ACC);
-}
-
-void push(int i) {
-	stack[++top] = i;
-}
-
-void shift(int i) {
-	push(i);
-	value[top] = yyval;
-	sym = yylex();
-}
-
-void reduce(int i) {
-	int old_top;
-	top -= prod_length[i];
-	old_top = top;
-	push(go_to[stack[old_top]][prod_left[i]]);
-
-	switch (i) {
-	case 1:
-		value[top] = value[old_top + 1] + value[old_top + 3];
-		break;
-	case 2:
-		value[top] = value[old_top + 1];
-		break;
-	case 3:
-		value[top] = value[old_top + 1] * value[old_top + 3];
-		break;
-	case 4:
-		value[top] = value[old_top + 1];
-		break;
-	case 5:
-		value[top] = value[old_top + 2];
-		break;
-	case 6:
-		value[top] = value[old_top + 1];
-		break;
-	default:
-		printf("parsing table error");
-		break;
-	}
-}
-
-void yyerror() {
-	printf("syntax error\n");
-	exit(1);
-}
-
-int yylex() {
-	static char ch = ' ';
-	char yytext[32];
-	int i = 0;
-	while (ch == ' ' || ch == '\n' || ch == '\t')
-		ch = getchar();
-	yyval = 0;
-	if (isdigit(ch)) {
-		do {
-			ch = yytext[i] = ch;
-			ch = getchar();
-		} while (isdigit(ch));
-		yyval = atoi(yytext);
-		yytext[i] = '0';
-		return (NUMBER);
-	}
-	else if (ch == '+') {
-		ch = getchar();
-		return(PLUS);
-	}
-	else if (ch == '*') {
-		ch = getchar();
-		return(STAR);
-	}
-	else if (ch == '(') {
-		ch = getchar();
-		return(LPAREN);
-	}
-	else if (ch == ')') {
-		ch = getchar();
-		return(RPAREN);
-	}
-	else if (ch == '$') {
-		return(END);
+	data result;
+	fgets(input, MAX, stdin);
+	get_token();
+	result = expression();
+	if (token != END) {
+		error(3);
 	}
 	else {
-		yyerror();
+		if (result.type == intNum) {
+			printf("%d\n", result.value.i);
+		}
+		else {
+			printf("%f\n", result.value.f);
+		}
 	}
+}
+
+data expression() {
+	data result;
+	result = term();
+	while (token == PLUS) {
+		get_token();
+		data rest = term();
+		if (rest.type == intNum && result.type == floatNum) {
+			result.value.f = result.value.f + (float)rest.value.i;
+		}
+		else if (rest.type == floatNum && result.type == intNum) {
+			result.type = floatNum;
+			int temp = result.value.i;
+			result.value.f = rest.value.f + (float)temp;
+		}
+		else if (rest.type == floatNum && result.type == floatNum) {
+			result.value.f = result.value.f + rest.value.f;
+		}
+		else if (rest.type == intNum && result.type == intNum) {
+			result.value.i = result.value.i + rest.value.i;
+		}
+	}
+	return result;
+}
+
+data term() {
+	data result;
+	result = factor();
+	while (token == STAR) {
+		get_token();
+		data rest = factor();
+		if (rest.type == intNum && result.type == floatNum) {
+			result.value.f = result.value.f + (float)rest.value.i;
+		}
+		else if (rest.type == floatNum && result.type == intNum) {
+			result.type = floatNum;
+			int temp = result.value.i;
+			result.value.f = rest.value.f + (float)temp;
+		}
+		else if (rest.type == floatNum && result.type == floatNum) {
+			result.value.f = result.value.f + rest.value.f;
+		}
+		else if (rest.type == intNum && result.type == intNum) {
+			result.value.i = result.value.i + rest.value.i;
+		}
+	}
+	return result;
+}
+
+data factor() {
+	data result;
+	if (token == NUMBER) {
+		if (num.type == intNum) {
+			result.type = intNum;
+			result.value.i = num.value.i;
+		}
+		else {
+			result.type = floatNum;
+			result.value.f = num.value.f;
+		}
+		get_token();
+	}
+
+	else if (token == LPAREN) {
+		get_token();
+		result = expression();
+		if (token == RPAREN) {
+			get_token();
+		}
+		else {
+			error(2);
+		}
+	}
+	else {
+		error(1);
+	}
+	return result;
+}
+
+//get_token()
+void get_token() {
+	int point = 0;
+	char array[30];
+	int count = 0;
+
+	//nexttoken ->tokrn
+	enum {
+		NUMBER = 0, PLUS = 1, STAR = 2, LPAREN = 3, RPAREN = 4, END = 5, INIT = 6
+	} nextToken;
+	nextToken = INIT;
+	//number value -> num
+	for (int i = current; i < strlen(input) + 1; i++) {
+		//다음토큰이 숫자이고 input[i]에 들어온 문자가 숫자이고 .이 아니면
+		if (nextToken == NUMBER && (isdigit(input[i]) == 0 && input[i] != '.')) {
+			current = i; 
+
+			if (point == 1) { //실수
+				num.type = floatNum;
+				num.value.f = atof(array);
+			} 
+			else { //정수
+				num.type = intNum;
+				num.value.i = atoi(array);
+			}
+			break;
+		}
+		if (input[i] == '+') { //+를 입력했을 때
+			nextToken = PLUS; //다음 토큰은 PLUS
+			current = i + 1; // current 증가
+			break;
+		}
+		else if (input[i] == '*') { //*를 입력했을 때
+			nextToken = STAR;
+			current = i + 1;
+			break;
+		}
+		else if (input[i] == '(') { //(를 입력했을 때
+			nextToken = LPAREN;
+			current = i + 1;
+			break;
+		}
+		else if (input[i] == ')') {  //)를 입력했을 때
+			nextToken = RPAREN;
+			current = i + 1;
+			break;
+		}
+		else if (input[i] == '.') {
+			point = 1;
+			array[count] = input[i];
+			count++;
+		}
+		else if (isdigit(input[i]) != 0) {
+			nextToken = NUMBER;
+			array[count] = input[i];
+			count++;
+		}
+		else if (input[i] == ' ') {
+			continue;
+		}
+		else {
+			nextToken = END;
+			break;
+		}
+
+	}
+	token = nextToken;
+}
+
+
+//에러문구 출력
+int error(int i) {
+	switch (i) {
+	case 1:
+		printf("error: number or '('\n");
+		break;
+	case 2:
+		printf("error: ')'\n");
+		break;
+	case 3:
+		printf("error: END\n");
+		break;
+	}
+	exit(1);
 }
