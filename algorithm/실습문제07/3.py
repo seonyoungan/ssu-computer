ad569e8cad30078336425b189875f6989f016d98
seonyoungan <asy093@naver.com>
2022-10-18 09:43:38 +0900

    add: algorithm 7-4

--- algorithm/실습문제07/3.py
+++ algorithm/실습문제07/3.py
@@ -1,7 +1,8 @@
-a = 1000 - int(input())
+a = 1000 - int(input()) #1000미만 정수
 b=[500, 100, 50, 10,5,1]
 count=0
 for i in b:
-    count+=a
-    a%=i
-print(count)
+    count+=a #잔돈누적
+    a%=i #나머지
+print(count) #잔돈
+
