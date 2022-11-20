ffe2b15fe612d447164a7e99875fb33dde663f77
안선영 <74126735+seonyoungan@users.noreply.github.com>
2022-11-21 05:34:41 +0900

    compiler: hw6

--- compiler/hw6/type.h
+++ compiler/hw6/type.h
@@ -1,4 +1,6 @@
-#define NIL 0
+#ifndef __TYPE_H__
+#define __TYPE_H__
+#define NIL 0 
 typedef enum {FALSE,TRUE} BOOLEAN;
 typedef enum e_node_name {
 	N_NULL,
@@ -38,10 +40,8 @@ typedef enum e_node_name {
 	N_EXP_AND,
 	N_EXP_OR,
 	N_EXP_ASSIGN,
-	
 	N_ARG_LIST,
 	N_ARG_LIST_NIL,
-	
 	N_STMT_LABEL_CASE,
 	N_STMT_LABEL_DEFAULT,
 	N_STMT_COMPOUND,
@@ -56,32 +56,54 @@ typedef enum e_node_name {
 	N_STMT_RETURN,
 	N_STMT_CONTINUE,
 	N_STMT_BREAK,
-	
 	N_FOR_EXP,
 	N_STMT_LIST,
 	N_STMT_LIST_NIL,
-	
 	N_INIT_LIST,
 	N_INIT_LIST_ONE,
 	N_INIT_LIST_NIL} NODE_NAME;
 
-typedef enum {T_NULL,T_ENUM,T_ARRAY,T_STRUCT,T_UNION,T_FUNC,T_POINTER,
-		T_VOID} T_KIND;
-typedef enum {Q_NULL,Q_CONST,Q_VOLATILE} Q_KIND;
-typedef enum {S_NULL,S_AUTO,S_STATIC,S_TYPEDEF,S_EXTERN,S_REGISTER} S_KIND;
-typedef enum {ID_NULL,ID_VAR,ID_FUNC,ID_PARM,ID_FIELD,ID_TYPE,ID_ENUM,
-		ID_STRUCT,ID_ENUM_LITERAL}  ID_KIND;
+typedef enum
+{
+	T_NULL,T_ENUM,T_ARRAY,T_STRUCT,T_UNION,T_FUNC,T_POINTER, T_VOID
+} T_KIND;
+	
+typedef enum
+{
+	Q_NULL,Q_CONST,Q_VOLATILE
+} Q_KIND;
 
-typedef struct s_node {
+typedef enum
+{
+	S_NULL,S_AUTO,S_STATIC,S_TYPEDEF,S_EXTERN,S_REGISTER
+} S_KIND;
+
+typedef enum
+{
+	ID_NULL,
+	ID_VAR,
+	ID_FUNC,
+	ID_PARM,
+	ID_FIELD,
+	ID_TYPE,
+	ID_ENUM,
+	ID_STRUCT,
+	ID_ENUM_LITERAL
+} ID_KIND;
+
+typedef struct s_node
+{
 	NODE_NAME name;
 	int line;
 	int value;
-	struct s_type  *type;
+	struct s_type *type;
 	struct s_node *llink;
 	struct s_node *clink;
-	struct s_node *rlink;} A_NODE;
+	struct s_node *rlink;
+} A_NODE;
 
-typedef struct s_type {
+typedef struct s_type
+{
 	T_KIND kind;
 	int size;
 	int local_var_size;
@@ -89,11 +111,12 @@ typedef struct s_type {
 	struct s_id *field;
 	struct s_node *expr;
 	int line;
-	BOOLEAN check; 
-	BOOLEAN prt; } A_TYPE;
+	BOOLEAN check;
+	BOOLEAN prt; 
+} A_TYPE;
 
-
-typedef struct s_id {
+typedef struct s_id
+{
 	char *name;
 	ID_KIND kind;
 	S_KIND specifier;
@@ -104,11 +127,22 @@ typedef struct s_id {
 	A_TYPE *type;
 	int line;
 	struct s_id *prev;
-	struct s_id *link;} A_ID;
+	struct s_id *link;
+} A_ID;
 
-typedef union {int i; float f; char c; char *s;} LIT_VALUE;
-typedef struct lit {int addr; A_TYPE *type; LIT_VALUE value;} A_LITERAL;
+typedef union {
+	int i; float f; char c; char *s;
+} LIT_VALUE;
+
+typedef struct lit {
+	int addr; A_TYPE *type; LIT_VALUE value;
+} A_LITERAL;
+
 typedef struct {
 	A_TYPE *type;
 	S_KIND stor;
-	int line;} A_SPECIFIER;
+	int line;
+} A_SPECIFIER;
+
+
+#endif
--- compiler/hw6/y.tab.h
+++ compiler/hw6/y.tab.h
@@ -35,8 +35,6 @@
    especially those whose name start with YY_ or yy_.  They are
    private implementation details that can be changed or removed.  */
 
-
-
 #ifndef YY_YY_Y_TAB_H_INCLUDED
 # define YY_YY_Y_TAB_H_INCLUDED
 /* Debug traces.  */
@@ -47,124 +45,131 @@
 extern int yydebug;
 #endif
 
-/* Token type.  */
+/* Token kinds.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
   enum yytokentype
   {
-    IDENTIFIER = 258,
-    TYPE_IDENTIFIER = 259,
-    FLOAT_CONSTANT = 260,
-    INTEGER_CONSTANT = 261,
-    CHARACTER_CONSTANT = 262,
-    STRING_LITERAL = 263,
-    PLUS = 264,
-    MINUS = 265,
-    PLUSPLUS = 266,
-    MINUSMINUS = 267,
-    BAR = 268,
-    AMP = 269,
-    BARBAR = 270,
-    AMPAMP = 271,
-    ARROW = 272,
-    SEMICOLON = 273,
-    LSS = 274,
-    GTR = 275,
-    LEQ = 276,
-    GEQ = 277,
-    EQL = 278,
-    NEQ = 279,
-    DOTDOTDOT = 280,
-    LP = 281,
-    RP = 282,
-    LB = 283,
-    RB = 284,
-    LR = 285,
-    RR = 286,
-    PERIOD = 287,
-    COMMA = 288,
-    EXCL = 289,
-    STAR = 290,
-    SLASH = 291,
-    PERCENT = 292,
-    ASSIGN = 293,
-    COLON = 294,
-    AUTO_SYM = 295,
-    STATIC_SYM = 296,
-    TYPEDEF_SYM = 297,
-    STRUCT_SYM = 298,
-    ENUM_SYM = 299,
-    SIZEOF_SYM = 300,
-    UNION_SYM = 301,
-    IF_SYM = 302,
-    ELSE_SYM = 303,
-    WHILE_SYM = 304,
-    DO_SYM = 305,
-    FOR_SYM = 306,
-    CONTINUE_SYM = 307,
-    BREAK_SYM = 308,
-    RETURN_SYM = 309,
-    SWITCH_SYM = 310,
-    CASE_SYM = 311,
-    DEFAULT_SYM = 312
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    IDENTIFIER = 258,              /* IDENTIFIER  */
+    TYPE_IDENTIFIER = 259,         /* TYPE_IDENTIFIER  */
+    INTEGER_CONSTANT = 260,        /* INTEGER_CONSTANT  */
+    FLOAT_CONSTANT = 261,          /* FLOAT_CONSTANT  */
+    CHARACTER_CONSTANT = 262,      /* CHARACTER_CONSTANT  */
+    STRING_LITERAL = 263,          /* STRING_LITERAL  */
+    PLUSPLUS = 264,                /* PLUSPLUS  */
+    MINUSMINUS = 265,              /* MINUSMINUS  */
+    ARROW = 266,                   /* ARROW  */
+    LSS = 267,                     /* LSS  */
+    GTR = 268,                     /* GTR  */
+    LEQ = 269,                     /* LEQ  */
+    GEQ = 270,                     /* GEQ  */
+    EQL = 271,                     /* EQL  */
+    NEQ = 272,                     /* NEQ  */
+    AMPAMP = 273,                  /* AMPAMP  */
+    BARBAR = 274,                  /* BARBAR  */
+    DOTDOTDOT = 275,               /* DOTDOTDOT  */
+    LP = 276,                      /* LP  */
+    RP = 277,                      /* RP  */
+    LB = 278,                      /* LB  */
+    RB = 279,                      /* RB  */
+    LR = 280,                      /* LR  */
+    RR = 281,                      /* RR  */
+    COLON = 282,                   /* COLON  */
+    PERIOD = 283,                  /* PERIOD  */
+    COMMA = 284,                   /* COMMA  */
+    EXCL = 285,                    /* EXCL  */
+    STAR = 286,                    /* STAR  */
+    SLASH = 287,                   /* SLASH  */
+    PERCENT = 288,                 /* PERCENT  */
+    AMP = 289,                     /* AMP  */
+    SEMICOLON = 290,               /* SEMICOLON  */
+    PLUS = 291,                    /* PLUS  */
+    MINUS = 292,                   /* MINUS  */
+    ASSIGN = 293,                  /* ASSIGN  */
+    AUTO_SYM = 294,                /* AUTO_SYM  */
+    BREAK_SYM = 295,               /* BREAK_SYM  */
+    CASE_SYM = 296,                /* CASE_SYM  */
+    CONTINUE_SYM = 297,            /* CONTINUE_SYM  */
+    DEFAULT_SYM = 298,             /* DEFAULT_SYM  */
+    DO_SYM = 299,                  /* DO_SYM  */
+    ELSE_SYM = 300,                /* ELSE_SYM  */
+    ENUM_SYM = 301,                /* ENUM_SYM  */
+    FOR_SYM = 302,                 /* FOR_SYM  */
+    IF_SYM = 303,                  /* IF_SYM  */
+    RETURN_SYM = 304,              /* RETURN_SYM  */
+    SIZEOF_SYM = 305,              /* SIZEOF_SYM  */
+    STATIC_SYM = 306,              /* STATIC_SYM  */
+    STRUCT_SYM = 307,              /* STRUCT_SYM  */
+    SWITCH_SYM = 308,              /* SWITCH_SYM  */
+    TYPEDEF_SYM = 309,             /* TYPEDEF_SYM  */
+    UNION_SYM = 310,               /* UNION_SYM  */
+    WHILE_SYM = 311                /* WHILE_SYM  */
   };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
-/* Tokens.  */
+/* Token kinds.  */
+#define YYEMPTY -2
+#define YYEOF 0
+#define YYerror 256
+#define YYUNDEF 257
 #define IDENTIFIER 258
 #define TYPE_IDENTIFIER 259
-#define FLOAT_CONSTANT 260
-#define INTEGER_CONSTANT 261
+#define INTEGER_CONSTANT 260
+#define FLOAT_CONSTANT 261
 #define CHARACTER_CONSTANT 262
 #define STRING_LITERAL 263
-#define PLUS 264
-#define MINUS 265
-#define PLUSPLUS 266
-#define MINUSMINUS 267
-#define BAR 268
-#define AMP 269
-#define BARBAR 270
-#define AMPAMP 271
-#define ARROW 272
-#define SEMICOLON 273
-#define LSS 274
-#define GTR 275
-#define LEQ 276
-#define GEQ 277
-#define EQL 278
-#define NEQ 279
-#define DOTDOTDOT 280
-#define LP 281
-#define RP 282
-#define LB 283
-#define RB 284
-#define LR 285
-#define RR 286
-#define PERIOD 287
-#define COMMA 288
-#define EXCL 289
-#define STAR 290
-#define SLASH 291
-#define PERCENT 292
+#define PLUSPLUS 264
+#define MINUSMINUS 265
+#define ARROW 266
+#define LSS 267
+#define GTR 268
+#define LEQ 269
+#define GEQ 270
+#define EQL 271
+#define NEQ 272
+#define AMPAMP 273
+#define BARBAR 274
+#define DOTDOTDOT 275
+#define LP 276
+#define RP 277
+#define LB 278
+#define RB 279
+#define LR 280
+#define RR 281
+#define COLON 282
+#define PERIOD 283
+#define COMMA 284
+#define EXCL 285
+#define STAR 286
+#define SLASH 287
+#define PERCENT 288
+#define AMP 289
+#define SEMICOLON 290
+#define PLUS 291
+#define MINUS 292
 #define ASSIGN 293
-#define COLON 294
-#define AUTO_SYM 295
-#define STATIC_SYM 296
-#define TYPEDEF_SYM 297
-#define STRUCT_SYM 298
-#define ENUM_SYM 299
-#define SIZEOF_SYM 300
-#define UNION_SYM 301
-#define IF_SYM 302
-#define ELSE_SYM 303
-#define WHILE_SYM 304
-#define DO_SYM 305
-#define FOR_SYM 306
-#define CONTINUE_SYM 307
-#define BREAK_SYM 308
-#define RETURN_SYM 309
-#define SWITCH_SYM 310
-#define CASE_SYM 311
-#define DEFAULT_SYM 312
+#define AUTO_SYM 294
+#define BREAK_SYM 295
+#define CASE_SYM 296
+#define CONTINUE_SYM 297
+#define DEFAULT_SYM 298
+#define DO_SYM 299
+#define ELSE_SYM 300
+#define ENUM_SYM 301
+#define FOR_SYM 302
+#define IF_SYM 303
+#define RETURN_SYM 304
+#define SIZEOF_SYM 305
+#define STATIC_SYM 306
+#define STRUCT_SYM 307
+#define SWITCH_SYM 308
+#define TYPEDEF_SYM 309
+#define UNION_SYM 310
+#define WHILE_SYM 311
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
@@ -176,6 +181,8 @@ typedef int YYSTYPE;
 
 extern YYSTYPE yylval;
 
+
 int yyparse (void);
+
 
 #endif /* !YY_YY_Y_TAB_H_INCLUDED  */
